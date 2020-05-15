f7b83fa6f85b385083dcb068f8d9cd227b09a41d
Thanh Pham <tpham0123@gmail.com>
2020-05-15 01:39:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
 
 
-def my_collect(collection)
-  a = [0]
+def my_collect(students)
+  names = [0]
   while 
     yield do 
   end
   
 end 
 
-collection = []
+names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
